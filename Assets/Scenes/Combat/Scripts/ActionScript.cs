b9e4ec1636b91936ef0351417e29c0e2617f7016
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-10-01 17:14:38 +0200

    feat: created template combat UI

---
+++ Assets/Scenes/Combat/Scripts/ActionScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class ActionScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Attack()
+    {
+        Debug.Log("The attack button was clicked!");
+        //TODO: attack
+    }
+
+    public void Item()
+    {
+        Debug.Log("The item button was clicked!");
+        //TODO: attack
+    }
+
+    public void Magic()
+    {
+        Debug.Log("The magic button was clicked!");
+        //TODO: attack
+    }
+
+    public void run()
+    {
+        Debug.Log("The run button was clicked!");
+        //TODO: attack
+    }
+}
