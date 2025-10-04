74e3634e8a43c4cbde40848288303eeaadad9e68
yuliia-sherstiuk <yuliia.sherstiuk@laplateforme.io>
2025-10-04 21:29:38 +0200

    Add BattleSystem class with core battle states, unit prefabs, and battle station setup for turn-based combat

--- Assets/Scenes/Combat/Scripts/BattleSystem.cs
+++ Assets/Scenes/Combat/Scripts/BattleSystem.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
+using System.Collections;
 
-public class NewEmptyCSharpScript
+
+public class BattleSystem : MonoBehaviour
 {
-    
+    public enum BattleState { START, PLAYER_TURN, ENEMY_TURN, WON, LOST, RUN }
+
+    [Header("Combat Station")]
+    public Transform playerBatleStation;//Player spawn point on the battlefileld
+    public Transform enemyBattleStation;//Enemy spawn point on the battlefield
+
+    [Header("Unit Prefabs")]
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+
+    private BattleState currentState;
+    private PlayerUnit playerUnit;
+    private EnemyUnit enemyUnit;
+    private GameObject playerInstance;
+    private GameObject enemyInstance;
+
 }
